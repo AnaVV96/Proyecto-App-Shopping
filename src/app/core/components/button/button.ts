5f7c78b89749ca4879a573613ad712c255342e7f
AnaVV96 <anavicentevillalonga@gmail.com>
2025-07-31 11:38:00 +0200

    arreglo de botones reutilizables

--- src/app/core/components/button/button.ts
+++ src/app/core/components/button/button.ts
@@ -1,17 +1,29 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+
+
 
 @Component({
   selector: 'app-button',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule],
   templateUrl: './button.html',
   styleUrl: './button.css'
 })
 export class Button {
-  @Input() label: string = "";
-  @Input() type: 'button' | 'submit' | 'reset' = 'button';
-  @Input() color: 'primary' | 'neutral' | 'accent' = 'primary';
+  @Input() label?: string;
+  @Input() level: 'primary' | 'secondary' = 'primary';
+  @Input() color?: string;
+  @Input() size: 'large' | 'small' = 'large';
+  @Input() type?: 'outline' | 'link';
   @Input() disabled: boolean = false;
+  @Input() icon?: string;
+  @Input() iconOnly: boolean = false;
+  @Input() iconColor: string = 'inherit';
+
+
+
+
 
   @Output() clicked = new EventEmitter<void>();
 
